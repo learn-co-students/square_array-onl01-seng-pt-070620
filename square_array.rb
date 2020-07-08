12a3de466d6121e1660be371fb0f415c80268e88
Denise Henderson <denise@henderson3.us>
2020-07-08 22:02:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  sqarray = []
+  array.each do |number|
+    #element = number**2
+    sqarray.push(number**2)
+    #puts array
+  end
+  return sqarray
 end
+
+#array = [1,2,3]
+#p square_array(array)
