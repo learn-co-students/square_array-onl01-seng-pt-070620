1d784a5117c2647ee4dfdd5bf0a31c08a386b42b
Landon <reasoner.landon90@gmail.com>
2020-07-13 23:16:58 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  squared = []
+  array.each do |number|
+    squared.push(number ** 2)
+  end
+  squared
 end
+
+
+# each always returns the original array
+# .collect or .map return a modified array based on whatever
+# was done in the block
