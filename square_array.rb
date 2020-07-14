7dc7630971d109fc4c6e705e2bf4d3dc2b74d24d
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-14 23:02:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  numbers = [1,2,3]
+  new_numbers = [9,10,16,25]
+  square_array(numbers)
+    return numbers.collect(&**)
+  square_array(new_numbers)
+    return new_numbers.collect(&**)
 end
+
