358d603a4b128b8e032f025484c2e430933f2822
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 07:24:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   # your code here
   new_array=[ ]
   array.each do |integer|
-    new_array << integer * integer 
+    new_array << integer ** 2 
     end
+    return new_array
   end
   
